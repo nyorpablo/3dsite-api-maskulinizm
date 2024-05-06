238579b9114de8e676b153bebfd1fa08135691d9
Paul Christian De Guzman <deguzmanpaulchristian@gmail.com>
2024-05-07 05:56:05 +0800

    added changes to layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,10 +18,14 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <livewire:welcome.navigation />
+        @if(request()->is('login') || request()->is('register'))
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div class="w-full sm:max-w-md px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        @endif
                 {{ $slot }}
+        @if(request()->is('login') || request()->is('register'))
             </div>
         </div>
+        @endif
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,6 @@
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-
                 <main class="mt-6">
                     <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                         <a
