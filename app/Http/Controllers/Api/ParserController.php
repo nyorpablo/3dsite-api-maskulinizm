6b79cfd0f9fd12430214ac23f66c95c25d9580ac
Paul Christian De Guzman <deguzmanpaulchristian@gmail.com>
2024-05-07 14:53:23 +0800

    storage put

--- app/Http/Controllers/Api/ParserController.php
+++ app/Http/Controllers/Api/ParserController.php
@@ -245,7 +245,7 @@ class ParserController extends Controller
             $fileContent = file_get_contents($file_path);
             $originalFilename = basename(parse_url($file_path, PHP_URL_PATH));
             $uniqueFilename = uniqid('odysse_3d_'.pathinfo($originalFilename, PATHINFO_FILENAME) . '_') . '.' . pathinfo($originalFilename, PATHINFO_EXTENSION);
-            $log = Storage::put('public/stl/' . $uniqueFilename, $fileContent);
+            Storage::put('public/stl/' . $uniqueFilename, $fileContent);
             $storedFileUrl = Storage::url('public/stl/' . $uniqueFilename);
             Log::info('Successfully added file: ' . $storedFileUrl);
         } catch (\Throwable $th) {
